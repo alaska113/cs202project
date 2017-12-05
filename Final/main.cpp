dd880c4ff1c4292c5fbbd62936d8ef1f0ff2c361
KSUA <kahowe@alaska.edu>
2017-12-05 11:10:52 -0900

    Turning in project. Everything is in the 'Final' directory.

---
+++ Final/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  
+//
+//  Created by KSUA on 11/15/17.
+//
+
+#include <stdio.h>
+#include "connect4.hpp"
+#include <iostream>
+#include <stdlib.h>
+
+int main()
+{
+    Game("Connect 4!");
+}
